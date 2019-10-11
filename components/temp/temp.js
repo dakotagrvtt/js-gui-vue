1b79ef953edcfd1e161775b9c37beb502fcedceb
Dakota Gravitt <dakotagrvtt50@gmail.com>
2019-10-11 12:47:46 -0500

    Added comments

--- components/temp/temp.js
+++ components/temp/temp.js
@@ -9,12 +9,15 @@ const adder = new Vue({
     result: function () {
       const celsius = parseFloat(this.degC)
       const kelvin = celsius + 273.15
+//display nothing if no input
       if(isNaN(kelvin)){
         return ``
       }
+//Error if kelvin is below 0
       else if(kelvin < 0){
         return `Nothing can go lower than Absolute Zero!`
       }
+//Normal calculation display
       else{
       return `is ${kelvin}°K`
       }
